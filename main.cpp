ca781c954a8363a15d5ebdcc566aedd71d8c11a4
pinchyCZN <pinchyCZN@gmail.com>
2015-04-10 07:23:50 -0500

    updates

--- main.cpp
+++ main.cpp
@@ -212,6 +212,33 @@ for(f=0; f<64; f++){
 //I i,j,k,m,n,o;D a,b,c,x,y;for(i=0;i<32;i++){{L{x=ix-W/2;y=iy-H/2;a=sqrt(x*x+y*y);if(a<70)B(y+H/2,x+H/2)=44;}}{L{x=ix-W/2;y=iy-H/2;c=1.7;b=.2+.8*sin(i*PI/16);b*=b;a=sqrt(x*x/c+y*y/b);if(a<50 && y>0)B(y+H/2+10,x+H/2)=0;}}for(j=0;j<2;j++){L{x=ix-W/2;y=iy-H/2;a=sqrt(x*x+y*y);if(a<10)B(y+H/2-20,x+H/2-20+40*j)=0;}}j=0;x=y=10;while(o="IBIB"[j]){for(k=0;k<64;k++){m=k%8;n=k/8;if(((U8*)0xFFA6E)[(o-1)*8+n]&(0x80>>m))B(y+n,x+m+j*8)=15;}j++;}C;}
 //I j,k,l,m;D i,a,x,y,z,cx,sx,tx,ty,tz;PG;for(i=0;i<32;i++){I p[]={-1,-1,1,-1,-1,1,1,1};D *t[]={&x,&y,&z,&y,&x,&z,&z,&x,&y};for(m=0;m<3;m++){for(j=0;j<12;j++)for(a=0;a<20;a+=.2){k=j%4;l=(j/4)*3;t[l][0]=a-10;t[l+1][0]=10*p[k*2];t[l+2][0]=10*p[k*2+1];cx=cos(i*PI/16+m/7.);sx=sin(i*PI/16+m/7.);tx=x;ty=y*cx+z*sx;tz=y*(-sx)+z*(cx);tz+=30+8*sin(i*PI/16);x=tx*80/tz;y=ty*80/tz;tz=(tz=1400-tz*tz)>255?255:tz<0?0:tz;B(y+H/2,x+W/2)=tz;}}C;}
 //I j;D i,a,b,c,x,y,m,rx,rz,z,sz,t[]={30,32,65,67};PG;rx=0;rz=0;sz=20;for(i=0;i<23;i++){for(rx=rz;rx<rz+sz;rx++){z=0;for(j=0;j<4;j+=2){if(rx>=PI*t[j] && rx<=PI*t[j+1])z=1;}if(z){for(a=rx;a<rx+1;a+=.1){b=80*sin(a);for(x=a-1;x<a+1;x++){for(y=0;y<H;y++){c=sqrt(pow(a-x,2)+pow(b+H/2-y,2));if(c<3){B(y,x)=255-c*30;}}}}}else{B(H/2,rx)=0xFF;{L{m=B(iy,ix);m-=2;if(m<0)m=0;B(iy,ix)=m;}}}}rz+=sz;F;}
+	if(0)
+	{
+		I j,x,y,a,b;
+		D m;
+		{L{B(iy,ix)=15;}}
+		j=0;x=100;y=10;
+		U8 *f=(U8*)DOSCHAR;
+		for(j=0;j<5;j++){
+		y=10;
+		x=110+j*20;
+		m=2.8;
+		for(a=0;a<8*m;a++)
+			for(b=0;b<8*m;b++)
+				if(f["SORNY"[j]*8+(I)(b/m)]&(0x80>>(I)(a/m)))
+					B(y+b,x+a)=104;
+		}
+		for(j=0;j<8;j++){
+		y=180;
+		x=110+j*14;
+		m=2.1;
+		for(a=0;a<8*m;a++)
+			for(b=0;b<8*m;b++)
+				if(f["BUTTHOLE"[j]*8+(I)(b/m)]&(0x80>>(I)(a/m)))
+					B(y+b,x+a)=104;
+		}
+		F;
+	}
 	{
 
 I i,j,k,a,b,x,y,w;
@@ -249,15 +276,6 @@ for(i=0;i<64;i++){
 		}
 
 	}
-	j=0;x=100;y=10;
-	while(w="SORNY"[j]) {
-        for(k=0; k<64; k++) {
-            a=k%8;
-            b=k/8;
-            if(((U8*)DOSCHAR)[w*8+b]&(0x80>>a))B(y+b,x+a+j*8)=1;
-        }
-        j++;
-    }
 	/*
 	U8 *f=(U8*)DOSCHAR;
 	for(j=0;j<5;j++){
